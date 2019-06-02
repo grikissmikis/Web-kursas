a70597eb2b796c99414027af1fcd48a1d8ec8bff
grikissmikis <gintare.salnikaite@gmail.com>
2019-06-02 12:27:16 +0300

    Pridetas PHP10 darbas

---
+++ PHP10/index10.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Formulė</title>
+    </head>
+    <body>
+        <?php
+        
+        function staciakampio_plotas ($a, $b) {
+            $plotas = $a * $b;
+            echo "Stačiakampio plotas: $plotas.";
+        }
+        
+        staciakampio_plotas(3, 10);
+        
+        ?>
+
+    </body>
+</html>
